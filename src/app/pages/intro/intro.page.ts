4ed4f21503bc8524d718c8ff9b81dae7c1550ef6
Asland3 <malthe3@outlook.com>
2023-10-04 22:01:39 +0200

    Added swiper to intro screen

--- src/app/pages/intro/intro.page.ts
+++ src/app/pages/intro/intro.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import Swiper from 'swiper';
 
 
 @Component({
@@ -6,17 +7,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
 })
-export class IntroPage implements OnInit {
-  
+export class IntroPage {
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined; 
+  swiper?: Swiper;
+
+
+
 
   constructor() { }
 
-  ngOnInit() {
+
+  swiperReady() {
+    // Wait for the component to render completely
+    setTimeout(() => {
+      this.swiper = this.swiperRef?.nativeElement.swiper;
+    }, 1000); 
   }
+
 
   next() {
-    // Logic for going to next slide here
+    this.swiper?.slideNext();
   }
 
+  skip() {
+    // Logic for skipping to the last slide here
+  }
+
+  swiperSlideChanged(e: any) {
+    console.log(e);
+  }
 
 }
